c4cee7b778747b34e33fb05fb6ad8d8bed97f689
Luella Fu <luellafu@Luellas-MacBook-Pro.local>
2016-05-07 16:43:17 -0700

    Added postgres

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var bluebird = require('bluebird');
 var bodyParser = require('body-parser');
 var plaid = require('plaid');
 var plaidHelper = require('./plaid.js');
+var pg = require('pg');
+
 
 //Environment
 var APP_PORT = "8000";
@@ -26,9 +28,12 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jade');
 
 //Routes
-require('./accounts/accounts.routes')(app);
+require('./accounts/accounts.routes')(app, pg);
 
 //Server
 var server = app.listen(APP_PORT, function () {
   console.log('BudgetCraft listening on port ' + String(APP_PORT));
 });
+
+
+
