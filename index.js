5b7959b03760a20e9eb3baee93c9917a642f56ef
Dushan Perera <dushan.v.perera@gmail.com>
2016-05-07 16:47:47 -0700

    Adding sessions

--- index.js
+++ index.js
@@ -3,10 +3,13 @@
 //Modules
 var envvar = require('envvar');
 var express = require('express');
+var session = require('express-session');
 var bluebird = require('bluebird');
 var bodyParser = require('body-parser');
 var plaid = require('plaid');
 var plaidHelper = require('./plaid.js');
+var pg = require('pg');
+
 
 //Environment
 var APP_PORT = "8000";
@@ -20,13 +23,14 @@ var plaidClient = new plaid.Client(PLAID_CLIENT_ID, PLAID_SECRET, plaid.environm
 var app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(session({resave: true, saveUninitialized: true, secret: 'SOMERANDOMSECRETHERE', cookie: { maxAge: 60000 }}));
 
 //Views
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade');
 
 //Routes
-require('./accounts/accounts.routes')(app);
+require('./accounts/accounts.routes')(app, pg);
 
 //Server
 var server = app.listen(APP_PORT, function () {
